6c7bdf275007e9e6f9dc369eea7918897e1b08c2
enes.cicek <enes.cicek@vbt.com.tr>
2022-10-04 14:37:39 +0300

    Repositories Added

---
+++ Blog.Repository/Concretes/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Core;
+using Blog.Dal.Concrete;
+using Blog.Entities;
+using Blog.Repository.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Repository.Concretes
+{
+    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(BlogContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Blog.Repository/Concretes/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Core;
+using Blog.Dal.Concrete;
+using Blog.Entities;
+using Blog.Repository.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Repository.Concretes
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(BlogContext context) : base(context)
+        {
+        }
+    }
+}
